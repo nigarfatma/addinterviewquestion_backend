8e5a208750ad3de4ec26e50842aba025b069e1ad
nigarfatma180 <nigarfatma180@gmail.com>
2023-05-01 23:52:21 +0530

    remove mongodb free cluster

--- db/conn.js
+++ db/conn.js
@@ -4,9 +4,7 @@ mongoose.set("strictQuery", true);
 mongoose.connect("mongodb://127.0.0.1/interview").then(() => {
     console.log("mongodb cluster connected sucessfully");
   })
-// mongoose.connect("mongodb+srv://nodejsinterview:Fatma76@cluster0.ythmig4.mongodb.net/nodejsinterview").then(() => {
-//   console.log("mongodb cluster connected sucessfully");
-// })
+
 
   .catch((err) => {
     //("no connection");
